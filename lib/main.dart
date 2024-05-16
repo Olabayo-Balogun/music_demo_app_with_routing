7b33149120e2ac436caa97e52463b2879253286f
Olabayo Balogun <olabayobalogun@gmail.com>
2024-05-16 20:51:53 +0100

    Added a bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -15,25 +15,48 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: DefaultTabController(
-        length: 2, // Number of tabs
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('Music Playlist'),
-            bottom: TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.music_note), text: 'Songs'),
-                Tab(icon: Icon(Icons.favorite), text: 'Liked'),
-              ],
-            ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _tabs = [
+    PlaylistScreen(),
+    LikedSongs(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Music Playlist'),
+      ),
+      body: _tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.music_note),
+            label: 'Songs',
           ),
-          body: TabBarView(
-            children: [
-              PlaylistScreen(),
-              LikedSongs(),
-            ],
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Liked',
           ),
-        ),
+        ],
       ),
     );
   }
